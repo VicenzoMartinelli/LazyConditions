49a7c2e252963ffd5af29736e0f96e12d696b21e
Vicenzo <vicenzo@sponte.com.br>
2019-08-30 08:57:45 -0300

    initial

---
+++ LazyConditions/Extensions/AsyncLinqExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LazyConditions.Extensions
+{
+    public static class AsyncLinqExtensions
+    {
+        public static async Task<bool> AnyAsync<T>(
+            this IEnumerable<T> source, Func<T, Task<bool>> func, bool invert = false)
+        {
+            foreach (var element in source)
+            {
+                var result = await func(element);
+
+                if (invert)
+                    result = !result;
+
+                if (result)
+                    return true;
+            }
+            return false;
+        }
+
+        public static async Task<bool> AllAsync<TSource>(
+            this IEnumerable<TSource> source, Func<TSource,
+                Task<bool>> predicate,
+                bool invert = false)
+        {
+            foreach (var item in source)
+            {
+                var result = await predicate(item);
+
+                if (invert)
+                    result = !result;
+
+                if (!result)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ LazyConditions/LazyTask.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace LazyConditions
+{
+    public class LazyTask<T>
+    {
+        private readonly Lazy<Task<T>> _task;
+
+        public LazyTask(Func<Task<T>> func)
+        {
+            _task = new Lazy<Task<T>>(func);
+        }
+
+        public static LazyTask<T> Create(Func<Task<T>> func) => new LazyTask<T>(func);
+
+        public Lazy<Task<T>> Get() => _task;
+
+        public Task Execute()
+        {
+            return _task.Value;
+        }
+        public Task ExecuteIfValueIsNotCreated()
+        {
+            if (_task.IsValueCreated)
+                return Task.CompletedTask;
+            else
+                return _task.Value;
+        }
+    }
+}
