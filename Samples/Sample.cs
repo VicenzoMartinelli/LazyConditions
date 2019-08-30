c12a347fe5a2ea9f9baa496e55ed98398f61ef49
Vicenzo <vicenzo@sponte.com.br>
2019-08-30 16:28:39 -0300

    samples

---
+++ Samples/Sample.cs
@@ -0,0 +1,27 @@
+﻿using LazyConditions;
+using System.Threading.Tasks;
+
+namespace Samples
+{
+    public class Sample
+    {
+        public async Task<bool> Validate()
+        {
+            return await Conditions.AllFalseAsync(
+                LazyTask<bool>.Create(() => Task.FromResult(false)).Get(),
+                LazyTask<bool>.Create(async () => await ValidateSomenthing()).Get(),
+                LazyTask<bool>.Create(async () => await ValidateAnoterThing()).Get()
+            );
+        }
+
+        private async Task<bool> ValidateSomenthing()
+        {
+            return true;
+        }
+
+        private async Task<bool> ValidateAnoterThing()
+        {
+            return false;
+        }
+    }
+}
